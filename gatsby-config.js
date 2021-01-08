d8507ab7d3159d15c497f6fb01e0c352b55431d4
Hidetaka Okamoto <info@wp-kyoto.net>
2021-01-08 19:57:06 +0900

    chore: remove manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,18 +78,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-markdown',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-source-npm-package-search`,
       options: {
         keywords: ['talkyjs'],
